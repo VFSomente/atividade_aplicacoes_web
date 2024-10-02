9f1aaf622e1e6b90d9c19acc118628b39c023838
Victor Fernandes <154020731+VFSomente@users.noreply.github.com>
2024-10-02 12:27:13 -0300

    atualizacão temporária

--- main_src/Assets/js/runas.js
+++ main_src/Assets/js/runas.js
@@ -1,26 +1,20 @@
-// Seleciona os dois botões
-const botoes = document.querySelectorAll(".botao-alternar");
-const conteudos = document.querySelectorAll(".conteudo");
+function teste(){
+    console.log("OK")
+}
 
-// Adiciona um evento de clique a cada botão
-botoes.forEach((botao, indice) => {
-    botao.addEventListener("click", () => {
-        // Remove a classe "ativo" do conteúdo atualmente ativo
-        const conteudoAtivo = document.querySelector(".conteudo.ativo");
-        if (conteudoAtivo) {
-            conteudoAtivo.classList.remove("ativo");
-        }
-
-        // Remove a classe "ativo" do botão atualmente ativo
-        const botaoAtivo = document.querySelector(".botao-alternar.ativo");
-        if (botaoAtivo) {
-            botaoAtivo.classList.remove("ativo");
-        }
-
-        // Adiciona a classe "ativo" ao botão clicado
-        botao.classList.add("ativo");
-
-        // Adiciona a classe "ativo" ao conteúdo correspondente
-        conteudos[indice].classList.add("ativo");
-    });
-});
+function showDiv(runa) {
+    let divs = document.querySelectorAll('.runa-desativada');
+    divs.forEach(div => div.classList.remove('runa-ativada'));
+    console.log("Olá")
+    switch (runa) {
+        case 'arvoreRunaPrincipal':
+            document.getElementById('arvoreRunaPrincipal').classList.add('runa-ativada');
+            break;
+        case 'arvoreRunaSecundaria':
+            document.getElementById('arvoreRunaSecundaria').classList.add('runa-ativada');
+            break;
+        case 'none':
+        default:
+            break;
+    }
+}
